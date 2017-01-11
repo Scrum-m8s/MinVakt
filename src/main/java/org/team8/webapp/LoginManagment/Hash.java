312143ff73b11c78fee01423542f38dc20d76e77
espen <espenoss@gmail.com>
2017-01-11 09:28:58 +0100

    Opprettet logincheck

--- src/main/java/org/team8/webapp/LoginManagment/Hash.java
+++ src/main/java/org/team8/webapp/LoginManagment/Hash.java
@@ -3,7 +3,6 @@ package org.team8.webapp.LoginManagment;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -13,10 +12,7 @@ import java.security.spec.InvalidKeySpecException;
  */
 public class Hash {
 
-    // Juster etter behov
     private static final int SALT_LENGTH = 32;
-    private static final int NUM_OF_ITERATIONS = 10;
-    private static final int KEY_LENGTH = 256;
 
     // Generer salt til hashfunsksjon, må lagres sammen med hashet passord
     public static byte[] generateSalt(){
@@ -32,12 +28,11 @@ public class Hash {
     // Hentet fra https://www.owasp.org/index.php/Hashing_Java
     // Antall iterasjoner burde være slik at det tar ca 1,5 sekund å hashe på server
     // keyLength burde være 256
-    // Generert hash er 64 byte
-    public static byte[] hashPassword( final char[] password, final byte[] salt) {
+    public static byte[] hashPassword( final char[] password, final byte[] salt, final int iterations, final int keyLength ) {
 
         try {
             SecretKeyFactory skf = SecretKeyFactory.getInstance( "PBKDF2WithHmacSHA512" );
-            PBEKeySpec spec = new PBEKeySpec( password, salt, NUM_OF_ITERATIONS, KEY_LENGTH);
+            PBEKeySpec spec = new PBEKeySpec( password, salt, iterations, keyLength );
             SecretKey key = skf.generateSecret( spec );
             byte[] res = key.getEncoded( );
             return res;
@@ -47,20 +42,6 @@ public class Hash {
         }
     }
 
-    // returnerer hash + salt som hexadesimal string med lengde på 128
-    // første 64 er hash, siste 64 er salt
-    public static String createHashedPassword(String password){
-
-        byte[] salt = generateSalt();
-        byte[] hashedPassword = hashPassword(password.toCharArray(), salt);
-
-        String saltString = DatatypeConverter.printHexBinary(salt);
-        String hashedPasswordString = DatatypeConverter.printHexBinary(hashedPassword);
-
-        return hashedPasswordString + saltString;
-    }
-
-
     public static void main(String[] args){
         String password = "123456";
         char[] pword = password.toCharArray();
@@ -69,7 +50,7 @@ public class Hash {
 
         System.out.println(salt.length);
 
-        byte[] hashet = hashPassword(pword, salt);
+        byte[] hashet = hashPassword(pword, salt, 10, 256);
 
         for(int i=0;i<hashet.length;i++){
             System.out.println(hashet[i]);
--- src/main/java/org/team8/webapp/LoginManagment/LoginCheck.java
+++ src/main/java/org/team8/webapp/LoginManagment/LoginCheck.java
@@ -1,48 +1,10 @@
 package org.team8.webapp.LoginManagment;
 
-import org.team8.webapp.User.User;
-import org.team8.webapp.User.UserDAO;
-
-import javax.xml.bind.DatatypeConverter;
-
-import java.util.Arrays;
-
-import static org.team8.webapp.LoginManagment.Hash.hashPassword;
-
 /**
  * Created by espen on 11.01.2017.
  */
-
 public class LoginCheck {
 
-    // Midlertidig til REST service er implementert
-    public static UserDAO dbUser = new UserDAO();
 
-    // Tar brukernavn og passord, skal returnere brukerrolle hvis lykkes, ellers -1
-    public static int validateCredentials(User user) {
 
-        User databaseUser = dbUser.getUserById(user.getUserId());
-
-        if (databaseUser == null) {
-            return -1;
-        } else {
-
-            String hashAndSalt = databaseUser.getPassword();
-            byte[] hash = DatatypeConverter.parseHexBinary(hashAndSalt.substring(0, 64));
-            byte[] salt = DatatypeConverter.parseHexBinary(hashAndSalt.substring(64));
-
-            byte[] passwordHash = hashPassword(user.getPassword().toCharArray(), salt);
-
-            if(Arrays.equals(hash, passwordHash)){
-                return databaseUser.getRole();
-            }else{
-                return -1;
-            }
-        }
-    }
-
-    public static void main(String[] args){
-        User u = new User("employee", "123");
-        System.out.println(validateCredentials(u));
-    }
 }
