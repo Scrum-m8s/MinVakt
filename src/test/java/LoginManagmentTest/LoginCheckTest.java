d7665c28c84384a31d343fa528ed679a05d442e5
espen <espenoss@gmail.com>
2017-01-11 15:21:23 +0100

    Flyttet hashing av passord til UserResource

--- src/test/java/LoginManagmentTest/LoginCheckTest.java
+++ src/test/java/LoginManagmentTest/LoginCheckTest.java
@@ -1,36 +1,8 @@
 package LoginManagmentTest;
 
-import org.junit.Test;
-import org.team8.webapp.User.User;
-
-import static org.junit.Assert.*;
-import static org.team8.webapp.LoginManagment.LoginCheck.*;
-
-
 /**
  * Created by espen on 11.01.2017.
  */
 public class LoginCheckTest {
-    @Test
-    public void testValidateCredentials() throws Exception {
-        User u = new User();
 
-        // Finnes i databasen
-        u.setUserId("canders");
-        u.setPassword("Anders123");
-
-        assertTrue(validateCredentials(u));
-
-        // Feil passord
-        u.setPassword("aksjbfhjjh");
-
-        assertFalse(validateCredentials(u));
-
-        // Finnes ikke i databasen
-        u.setUserId("aslflbaglb");
-        u.setPassword("Anders123");
-
-        assertFalse(validateCredentials(u));
-
-    }
 }
