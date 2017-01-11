2beb72fc491f6e98fc1b28531f4471ab03e5294f
espen <espenoss@gmail.com>
2017-01-11 10:49:16 +0100

    Begynt arbeid på login

---
+++ src/main/java/org/team8/webapp/LoginManagment/User.java
@@ -0,0 +1,17 @@
+package org.team8.webapp.LoginManagment;
+
+/**
+ * Created by espen on 11.01.2017.
+ */
+public class User {
+    String userName;
+    String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
