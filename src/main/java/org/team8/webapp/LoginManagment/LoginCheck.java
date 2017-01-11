650c02d605c3aa642c7d8366c3b74e3c524fa0b7
espen <espenoss@gmail.com>
2017-01-11 10:49:16 +0100

    Begynt arbeid på login

--- src/main/java/org/team8/webapp/LoginManagment/LoginCheck.java
+++ src/main/java/org/team8/webapp/LoginManagment/LoginCheck.java
@@ -1,10 +1,32 @@
 package org.team8.webapp.LoginManagment;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by espen on 11.01.2017.
  */
+
+ @Path("/login")
 public class LoginCheck {
 
+    public LoginDAO login = new LoginDAO();
+
+    // Tar brukernavn og passord, skal returnere token hvis lykkes, ellers -1
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public int login(User user){
+
+        if(login.validateLogin(user.getUserName(), user.getPassword())){
+
+            // TODO: generer token
+
+            return 123;
+        }else{
+            return -1;
+        }
+    }
 
 
 }
