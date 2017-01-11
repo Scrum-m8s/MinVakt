83b29552df9e047612a1670e541edce329697fe8
HugoHappy <kristoffer96@gmail.com>
2017-01-11 13:59:26 +0100

    Trying to get mockito to work with travis

--- src/main/java/DatabaseTest/PortfolioTester.java
+++ src/main/java/DatabaseTest/PortfolioTester.java
@@ -1,5 +1,7 @@
 package DatabaseTest;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,7 @@ public class PortfolioTester {
     Portfolio portfolio;
     StockService stockService;
 
+    @Test
     public static void main(String[] args){
         PortfolioTester tester = new PortfolioTester();
         tester.setUp();
