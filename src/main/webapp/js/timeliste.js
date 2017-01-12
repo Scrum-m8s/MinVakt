f1ef00ccf992d51acad28e9d55f9291e2f9edf9c
AkuMegami <elenafalk@gmail.com>
2017-01-12 09:16:53 +0100

    modules.xml

---
+++ src/main/webapp/js/timeliste.js
@@ -0,0 +1,6 @@
+/*
+legg til event listener på hver vakt som blir opprettet. når vakten klikkes skal modal vises med
+relevant informasjon om vakten
+
+fargekode vaktene etter type vakt. feks gul = dag, mørkeblå = natt og grønn = kveld
+ */
