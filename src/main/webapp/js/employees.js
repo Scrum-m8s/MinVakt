5d693f91255b849041b43afc8613acb623e5d0c3
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2017-01-13 14:26:01 +0100

    employees.js

--- src/main/webapp/js/employees.js
+++ src/main/webapp/js/employees.js
@@ -25,7 +25,6 @@ $(document).ready(function(){
 
 
 
-
      $("#createEmployee").click(function () {
         $.ajax({
             url: 'api/employees',
@@ -49,4 +48,29 @@ $(document).ready(function(){
              }
          });
      });
+
+     $("#updateEmployee").click(function(){
+     $.ajax({
+         url: 'api/employees/id',
+         type: 'PUT',
+         data: JSON.stringify({
+             //user_id: $("#new_user_id").val(),
+             firstname: $("#new_firstname").val(),
+             surname: $("#new_surname").val(),
+             email: $("#new_email").val(),
+             phone_number: $("#new_phone_number").val(),
+             category: $("#new_category").val()
+
+         }),
+         contentType: 'application/json; charset=utf-8',
+         dataType: 'json',
+         success: function() {
+             alert("New employee was registered");
+         },
+         error: function() {
+             alert("New employee was not registered")
+         }
+     });
+     });
+
  })
