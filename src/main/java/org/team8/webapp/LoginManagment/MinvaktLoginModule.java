924905a3bf09e73097f72e6f1e6b3150fe298163
espen <espenoss@gmail.com>
2017-01-13 09:18:32 +0100

    Implementert grunnleggende login system

---
+++ src/main/java/org/team8/webapp/LoginManagment/MinvaktLoginModule.java
@@ -0,0 +1,97 @@
+package org.team8.webapp.LoginManagment;
+
+import org.team8.webapp.User.User;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.*;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.team8.webapp.LoginManagment.LoginCheck.validateCredentials;
+
+/**
+ * Created by espen on 12.01.2017.
+ */
+public class MinvaktLoginModule implements LoginModule {
+
+    private CallbackHandler handler;
+    private Subject subject;
+    private UserPrincipal userPrincipal;
+    private RolePrincipal rolePrincipal;
+    private String login;
+    private List<String> userGroups;
+
+    @Override
+    public void initialize(Subject subject,
+                           CallbackHandler callbackHandler,
+                           Map<String, ?> sharedState,
+                           Map<String, ?> options){
+        handler = callbackHandler;
+        this.subject = subject;
+
+    }
+
+    @Override
+    public boolean login() throws LoginException{
+
+        Callback[] callbacks = new Callback[2];
+        callbacks[0] = new NameCallback("login");
+        callbacks[1] = new PasswordCallback("password", true);
+
+        try{
+            handler.handle(callbacks);
+            String name = ((NameCallback) callbacks[0]).getName();
+            String password = String.valueOf(((PasswordCallback)callbacks[1]).getPassword());
+
+            User u = new User();
+            u.setUserId(name);
+            u.setPassword(password);
+
+            if(validateCredentials(u)){
+                login = name;
+                userGroups = new ArrayList<String>();
+                userGroups.add("admin");
+                return true;
+            }
+
+            throw new LoginException("Authentification failed");
+
+        }catch (IOException e) {
+            throw new LoginException(e.getMessage());
+        }catch(UnsupportedCallbackException e){
+            throw new LoginException(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean commit() throws LoginException{
+
+        userPrincipal = new UserPrincipal(login);
+        subject.getPrincipals().add(userPrincipal);
+
+        if(userGroups != null && userGroups.size() > 0){
+            for(String groupName : userGroups){
+                rolePrincipal = new RolePrincipal(groupName);
+                subject.getPrincipals().add(rolePrincipal);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean abort() throws LoginException{
+        return false;
+    }
+
+    @Override
+    public boolean logout() throws LoginException{
+        subject.getPrincipals().remove(userPrincipal);
+        subject.getPrincipals().remove(rolePrincipal);
+        return true;
+    }
+
+}
