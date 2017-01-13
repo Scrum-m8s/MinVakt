171d680d716601e12852c90c051a3ae061050203
espen <espenoss@gmail.com>
2017-01-13 12:48:52 +0100

    Implementert grunnleggende login system

--- src/main/java/org/team8/webapp/LoginManagment/DummyPrivilegedResource.java
+++ src/main/java/org/team8/webapp/LoginManagment/DummyPrivilegedResource.java
@@ -1,5 +1,7 @@
 package org.team8.webapp.LoginManagment;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,9 +13,27 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/dummy")
 public class DummyPrivilegedResource {
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String displayPrivilegedText(){
-        return "Secret";
+        return "You have general user privilege";
     }
+
+    @RolesAllowed("admin")
+    @GET
+    @Path("/admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String displayAdminPrivilegedText(){
+        return "You have admin privilege";
+    }
+
+    @RolesAllowed("employee")
+    @GET
+    @Path("/employee")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String displayEmployeePrivilegedText(){
+        return "You have employee privilege";
+    }
+
 }
