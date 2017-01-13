7e00c638ea5b9ef39b4c4d0b2b9a5b186f3490a1
espen <espenoss@gmail.com>
2017-01-13 09:18:32 +0100

    Implementert grunnleggende login system

--- src/main/java/org/team8/webapp/LoginManagment/DummyPrivilegedResource.java
+++ src/main/java/org/team8/webapp/LoginManagment/DummyPrivilegedResource.java
@@ -1,13 +1,9 @@
 package org.team8.webapp.LoginManagment;
 
-import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
 
 /**
  * Created by espen on 12.01.2017.
@@ -15,27 +11,9 @@ import javax.ws.rs.core.SecurityContext;
 
 @Path("/dummy")
 public class DummyPrivilegedResource {
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String displayPrivilegedText(@Context SecurityContext sc){
-        return "Hello, "+ sc.getUserPrincipal().getName() + ", you have general user privilege";
+    public String displayPrivilegedText(){
+        return "Secret";
     }
-
-    @RolesAllowed("admin")
-    @GET
-    @Path("/admin")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String displayAdminPrivilegedText(){
-        return "You have admin privilege";
-    }
-
-    @RolesAllowed("employee")
-    @GET
-    @Path("/employee")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String displayEmployeePrivilegedText(){
-        return "You have employee privilege";
-    }
-
 }
