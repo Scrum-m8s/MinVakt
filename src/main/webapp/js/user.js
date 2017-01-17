b378262cf6829cbb23d817e67592d676e888e54a
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2017-01-17 16:17:17 +0100

    javascript function to change password

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -2,37 +2,9 @@
  * Created by mariyashchekanenko on 13/01/2017.
  */
 
+
 $(document).ready(function() {
 
-
-    function registerUser(user_id, password, role, firstname, surname, phone_number, email, category) {
-
-        $.ajax({
-            type: "POST",
-            url: "api/users",
-            data: '{"userId": "' + user_id + '", "password" : "' + password + '", "role" : "' + role + '"}',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            success: function (data) {
-                $('#UserTable').DataTable().ajax.reload();
-                alert("New user was registered");
-            }
-        });
-        $.ajax({
-            type: 'POST',
-            url: 'api/employees',
-            data: '{"userId": "' + user_id + '", "firstname" : "' + firstname + '", "surname" : "' + surname + '", "phone_number" : "' + phone_number + '", "email" : "' + email + '", "category" : "' + category + '"}',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            success: function (data) {
-                alert("Employee was registered");
-
-            },
-            error: function() {
-            alert("Something went wrong");
-        }
-        });
-    }
 
     $("#regButton").click(function () {
         var user_id = $("#user_id").val();
@@ -49,7 +21,15 @@ $(document).ready(function() {
     });
 
 
-    // Slett
+    $("#changePasswordButton").click(function () {
+        var user_id = $("#user_id").val();
+        var new_password1 = $("#new_password1").val();
+        var new_password2 = $("#new_password2").val();
+        changePassword(user_id, new_password1, new_password2);
+
+    });
+
+    // Slett brukeren
     $("#deleteUser").click(function () {
         $.ajax({
             url: 'api/users/' + $("#deleteUserId").val(),
@@ -67,11 +47,70 @@ $(document).ready(function() {
 
 });
 
+//endre passord
+function changePassword(user_id, new_password1, new_password2) {
+    if(user_id == "") {
+        alert("Error: Username cannot be blank!");
+        user_id.focus();
+        return false;
+    }
+    if(new_password1 != "" && new_password1 == new_password2) {
+        if(new_password1.length < 8) {
+            alert("Error: Password must contain at least eight characters!");
+            $("#new_password1").focus();
+            return false;
+        }
+        re = /[0-9]/;
+        if(!re.test(new_password1)) {
+            alert("Error: password must contain at least one number (0-9)!");
+            $("#new_password1").focus();
+            return false;
+        }
+    } else {
+        alert("Error: Please check that you've entered and confirmed your password!");
+        $("#new_password1").focus();
+        return false;
+    }
+
+    alert("You entered a valid password: " + new_password1);
+
+    $.ajax({
+        type: "PUT",
+        url: "api/users",
+        data: '{"userId": "' + user_id + '", "password" : "' + new_password1 + '", "role" : "' + 1 + '"}',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+
+    });
+    return true;
 
 
+}
 
+function registerUser(user_id, password, role, firstname, surname, phone_number, email, category) {
 
+    $.ajax({
+        type: "POST",
+        url: "api/users",
+        data: '{"userId": "' + user_id + '", "password" : "' + password + '", "role" : "' + role + '"}',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (data) {
+            alert("New user was registered");
+        }
+    });
+    $.ajax({
+        type: 'POST',
+        url: 'api/employees',
+        data: '{"userId": "' + user_id + '", "firstname" : "' + firstname + '", "surname" : "' + surname + '", "phone_number" : "' + phone_number + '", "email" : "' + email + '", "category" : "' + category + '"}',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (data) {
+            alert("Employee was registered");
 
+        }
+    });
+}
 
 
 
