190c191a65d96bc65c2c7cdf27bc177c48c0a3f2
espen <espenoss@gmail.com>
2017-01-17 11:57:21 +0100

    laget service for å hente gjeldende bruker, satt opp tillatelser for brukertjenester, satt opp feilsider

--- src/main/java/org/team8/webapp/User/UserResource.java
+++ src/main/java/org/team8/webapp/User/UserResource.java
@@ -1,7 +1,9 @@
 package org.team8.webapp.User;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 import java.util.ArrayList;
 
 import static org.team8.webapp.LoginManagment.Hash.createHashedPassword;
@@ -18,6 +20,14 @@ public class UserResource {
     public ArrayList<User> getUsers() {
         System.out.println("getUsers");
         return dao.getUsers();
+    }
+
+    @Path("/current")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public User getCurrentUser(@Context SecurityContext sc){
+        System.out.println("getCurrentUser");
+        return dao.getUserById(sc.getUserPrincipal().getName());
     }
 
     @Path("{id}")
@@ -37,7 +47,6 @@ public class UserResource {
         return dao.createUser(e);
     }
 
-    @Path("{id}")
     @PUT
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public boolean updateUser(User e) {
