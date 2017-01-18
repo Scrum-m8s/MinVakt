a6fb329e2358789d333ee3a1edec8e9186c7e9c4
AkuMegami <elenafalk@gmail.com>
2017-01-18 13:54:42 +0100

    shift_book lister ut alle shift med fargekoder

--- src/main/webapp/js/shift_book_script.js
+++ src/main/webapp/js/shift_book_script.js
@@ -1,53 +1,48 @@
 
-$(document).ready(function initialize() {
 //fetch the shifts for all users and insert into correct dates in the calendar
+$(document).ready(function initialize() {
     $.get("/api/shift_lists/", function(data){
-        //data er her en liste over alle shiftlistene
+        for(var j = 0; j < data.length; ++j) { //shift counter
 
-            for(var j = 0; j < data.length; ++j) { //shift counter
+            var newDate = new Date(data[j].my_date); //the date of each shift
+            var first = new Date(newDate.getFullYear(), newDate.getMonth(), 1);
+            var currentWOM = newDate.getWeek() - first.getWeek() + 2;
 
-                var newDate = new Date(data[j].my_date); //the date of each shift
-                var first = new Date(newDate.getFullYear(), newDate.getMonth(), 1);
-                var currentWOM = newDate.getWeek() - first.getWeek() + 2;
-                var calDate = new Date();
-
-                if(newDate.getDay() != 0) {
-                    calDate = new Date(document.getElementById("r" + currentWOM + "c" + newDate.getDay() + 1));
-                } else{
-                    calDate = new Date(document.getElementById("r" + currentWOM + "c8"));
-                }
-
-                var shiftType = data[j].shift_id;
-                var color;
-                if(shiftType == 1){
-                    color = "DarkSlateBlue ";
-                } else if(shiftType ==2){
-                    color = "GoldenRod";
-                } else if(shiftType == 3){
-                    color = "LightSlateGrey";
-                }
-
-                var currentDiv;
-                var newDayContent;
-
-                //TODO: modal when clicked
-                if(calDate.getDay() != 0){
-                    currentDiv = document.getElementById("r" + currentWOM + "c" + (newDate.getDay() + 1));
-                    currentDiv.style.backgroundColor = color;
-                    newDayContent = document.createTextNode(newDate.toDateString());
-                    currentDiv.appendChild(newDayContent);
-                    currentDiv.insertBefore(newDayContent, null);
-                } else {
-                    currentDiv = document.getElementById("r" + currentWOM + "c8");
-                    currentDiv.style.backgroundColor = color;
-                    newDayContent = document.createTextNode(newDate.toDateString());
-                    currentDiv.appendChild(newDayContent);
-                    currentDiv.insertBefore(newDayContent, null);
-                    ++currentWOM;
-                }
+            var shiftType = data[j].shift_id;
+            var color;
+            if(shiftType == 1){
+                color = "DarkSlateBlue ";
+            } else if(shiftType ==2){
+                color = "GoldenRod";
+            } else if(shiftType == 3){
+                color = "LightSlateGrey";
             }
+
+            var currentDiv;
+            var newDayDiv;
+            var newDayContent;
+
+            //TODO: modal when clicked
+            if(newDate.getDay() != 0){
+                currentDiv = document.getElementById("r" + currentWOM + "c" + (newDate.getDay() + 1));
+                newDayDiv = document.createElement("div");
+                newDayContent = document.createTextNode(newDate.toDateString());
+                newDayDiv.appendChild(newDayContent);
+                newDayDiv.style.backgroundColor = color;
+                currentDiv.insertBefore(newDayDiv, null);
+            } else {
+                currentDiv = document.getElementById("r" + currentWOM + "c8");
+                newDayDiv = document.createElement("div");
+                newDayContent = document.createTextNode(newDate.toDateString());
+                newDayDiv.appendChild(newDayContent);
+                newDayDiv.style.backgroundColor = color;
+                currentDiv.insertBefore(newDayDiv, null);
+                ++currentWOM;
+            }
+        }
     });
 });
+//currentDiv.childNodes[0].innerHTML = "";
 
 /*
  "I suggest you use drop-downs or some similar constrained form of date entry rather than text boxes,
