af876db0ff8d3e956330e29f4daf0fc8516deef0
Håkon Paulsen <haakonpaulsen1234@hotmail.com>
2017-01-18 10:03:09 +0100

    small change in UserResource

--- src/main/java/org/team8/webapp/User/UserResource.java
+++ src/main/java/org/team8/webapp/User/UserResource.java
@@ -54,14 +54,15 @@ public class UserResource {
         return dao.updateUser(e);
     }
 
+    /* TODO:
     @Path("updateRole/{role}")
     @PUT
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public boolean updateRole(User e) {
         System.out.println("updateRole");
         return dao.updateRole(e);
     }
+    */
 
     @Path("{id}")
     @DELETE
