47010fabff2f529de0c874321d412994a706cec5
AkuMegami <elenafalk@gmail.com>
2017-01-18 12:27:34 +0100

    timeliste med fargekoder

--- src/main/webapp/js/timeliste.js
+++ src/main/webapp/js/timeliste.js
@@ -4,7 +4,7 @@ relevant informasjon om vakten
 
 fargekode vaktene etter type vakt. feks gul = dag, mørkeblå = natt og grønn = kveld
  */
-var user = "haakonrp"; //test
+var user = "anders"; //test value
 
 $(document).ready(function initialize() {
 //fetch the shifts for this user and insert into correct dates in the calendar
@@ -16,23 +16,32 @@ $(document).ready(function initialize() {
             var currentWOM = newDate.getWeek() - first.getWeek() + 2;
             var calDate = new Date();
             if(newDate.getDay() != 0) {
-                calDate = new Date(document.getElementById("r" + currentWOM + "c" + newDate.getDay() + 1)); //toDateString?
+                calDate = new Date(document.getElementById("r" + currentWOM + "c" + newDate.getDay() + 1));
             } else{
-                calDate = new Date(document.getElementById("r" + currentWOM + "c8")); //toDateString?
+                calDate = new Date(document.getElementById("r" + currentWOM + "c8"));
             }
-            var newCalDiv = document.createElement("div");
-            var newCalContent = document.createTextNode(newDate.toDateString() +" " + data[i].want_swap);
-            newCalDiv.appendChild(newCalContent);
+
+            var shiftType = data[i].shift_id;
+            var color;
+            if(shiftType == 1){
+                color = "DarkSlateBlue ";
+            } else if(shiftType ==2){
+                color = "GoldenRod";
+            } else if(shiftType == 3){
+                color = "LightSlateGrey";
+            }
             var currentDiv;
 
+            //TODO: modal when clicked
+            //TODO: change color depending on shift type
             if(calDate.getDay() != 0){
                 currentDiv = document.getElementById("r" + currentWOM + "c" + (newDate.getDay() + 1));
-                currentDiv.style.backgroundColor = "Brown";
-                currentDiv.insertBefore(newCalDiv, null);
+                currentDiv.innerHTML = newDate.toDateString(); // can add data[i].want_swap);
+                currentDiv.style.backgroundColor = color;
             } else {
                 currentDiv = document.getElementById("r" + currentWOM + "c8");
-                currentDiv.style.backgroundColor = "Brown";
-                currentDiv.insertBefore(newCalDiv, null);
+                currentDiv.innerHTML = newDate.toDateString();
+                currentDiv.style.backgroundColor = color;
                 ++currentWOM;
             }
         }
