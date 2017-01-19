cc872acab1d27ad4e56d354e2eab2d9fd95f97f8
HugoHappy <kristoffer96@gmail.com>
2017-01-19 16:09:22 +0100

    Starting with adding content-cards

--- src/main/webapp/js/taskbar.js
+++ src/main/webapp/js/taskbar.js
@@ -1,21 +1,35 @@
 /**
  * Created by KristofferLaptop on 17-Jan-17.
  */
-function openCloseNav() {
-    var sidebar = $("#sidebar-wrapper-id");
 
+var sidebar;
+
+function openCloseNav() {
+    sidebar = $("#sidebar-wrapper-id");
     if(sidebar.width()=="0"){
         $("#menu-icon-id").html('close');
         sidebar.css('width', '300');
+        alignCards();
     }else {
         $("#menu-icon-id").html('menu');
         sidebar.css('width', '0');
+        alignCards();
     }
 }
 
 $(document).ready(function(){
+    sidebar = $("#sidebar-wrapper-id");
     if(screen.height > "750"){
         $("#menu-icon-id").html('close');
-        $("#sidebar-wrapper-id").css('width', '300');
+        sidebar.css('width', '300');
     }
+    alignCards();
 });
+
+function alignCards(){
+    sidebar = $("#sidebar-wrapper-id");
+    var leftPush = sidebar.width()+8;
+    console.log(leftPush);
+    $("#content-wrapper-id").css('padding-left', leftPush);
+
+}
