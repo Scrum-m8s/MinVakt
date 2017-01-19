53b0bf498e5fe2f549c93009219931c869b8850d
Elena Nordmark <elenafalk@gmail.com>
2017-01-19 15:57:08 +0100

    week view on front page

--- src/main/webapp/js/kalenderuke.js
+++ src/main/webapp/js/kalenderuke.js
@@ -2,8 +2,6 @@
 $(document).ready(function initialize() {
 //current date
     var today = new Date();
-    var mnd = today.getMonth();
-    var last = new Date(today.getYear(), mnd + 1, 0);
 
 //get week of the year by date
     Date.prototype.getWeek = function () {
@@ -11,9 +9,6 @@ $(document).ready(function initialize() {
         var millisecsInDay = 86400000; // 1000*60*60*24 ms-s-min-hr
         return Math.ceil((((this - onejan) / millisecsInDay) + onejan.getDay() + 1) / 7);
     };
-
-    var first = new Date(today.getFullYear(), mnd, 1);
-    var firstWeek = first.getWeek();
 
 //TODO: switch week
 //creates an empty 1x8 grid with IDs
@@ -32,34 +27,26 @@ $(document).ready(function initialize() {
     newEndCol.className = "col-sm-4";
     document.getElementById("r1").appendChild(newEndCol);
 
+    var firstOfWeek = today.getDate() - today.getDay();
+    var tomorrow;
 
-    var currentDay = first;
-
-//TODO: write in a smaller date format
-//populate a div for each day of the month. last.getDate is the amount of days current month
-    for (var k = 0; k < 7; ++k) {
+//populate grid
+    for (var k = 1; k < 8; ++k) {
+        tomorrow = new Date(today.getFullYear(), today.getMonth(), (firstOfWeek + k));
         var newDay = document.createElement("div");
-        var newDayContent = document.createTextNode(currentDay.toDateString());
+        var newDayContent = document.createTextNode(tomorrow.toDateString());
         newDay.appendChild(newDayContent);
-        var currentDiv;
 
-        if (currentDay.getDay() != 0) {
-            currentDiv = document.getElementById("r1c" + (currentDay.getDay() + 1));
-        } else {
-            currentDiv = document.getElementById("r1c8");
-        }
+        var currentDiv = document.getElementById("r1c" + (k + 1));
         currentDiv.insertBefore(newDay, null);
-        currentDay.setDate(currentDay.getDate() + 1); //increment currentDay -> tomorrow
         currentDiv.style.backgroundColor = "AliceBlue";
     }
 
 
 //generate the week number and insert it in the grid
         var newWeekDiv = document.createElement("div");
-        var newWeekContent = document.createTextNode(firstWeek);
-        newWeekDiv.appendChild(newWeekContent); //add the text node to newly created div
-
-        // add the newly created element and its content into the DOM
+        var newWeekContent = document.createTextNode(today.getWeek() + 1);
+        newWeekDiv.appendChild(newWeekContent);
         var currentWeekDiv = document.getElementById("r1c1");
         currentWeekDiv.insertBefore(newWeekDiv, null);
 });
