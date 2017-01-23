31937193346a18b2b14efa4ca4242032f5710a9c
HugoHappy <kristoffer96@gmail.com>
2017-01-23 17:17:30 +0100

    Added angularJS material

--- src/main/webapp/js/taskbar.js
+++ src/main/webapp/js/taskbar.js
@@ -1,35 +1,130 @@
-/**
- * Created by KristofferLaptop on 17-Jan-17.
- */
+var app = angular.module('MinVakt', ['ngMaterial']);
 
-var sidebar;
 
-function openCloseNav() {
-    sidebar = $("#sidebar-wrapper-id");
-    if(sidebar.width()=="0"){
-        $("#menu-icon-id").html('close');
-        sidebar.css('width', '300');
-        alignCards();
-    }else {
-        $("#menu-icon-id").html('menu');
-        sidebar.css('width', '0');
-        alignCards();
-    }
-}
-
-$(document).ready(function(){
-    sidebar = $("#sidebar-wrapper-id");
-    if(screen.height > "750"){
-        $("#menu-icon-id").html('close');
-        sidebar.css('width', '300');
-    }
-    alignCards();
+app.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default').primaryPalette('blue');
 });
 
-function alignCards(){
-    sidebar = $("#sidebar-wrapper-id");
-    var leftPush = sidebar.width()+8;
-    console.log(leftPush);
-    $("#content-wrapper-id").css('padding-left', leftPush);
+app.controller('MinVaktCtrl', function($scope, $mdSidenav, $mdToast, $http) {
 
-}
+    $http.get('api/employees/current').
+    then(function(response) {
+        $scope.currentuser = response.data;
+    });
+
+    $scope.toggleSidenav = function(menu) {
+        $mdSidenav(menu).toggle();
+    };
+    $scope.toast = function(message) {
+        var toast = $mdToast.simple().content('You clicked ' + message).position('bottom right');
+        $mdToast.show(toast);
+    };
+    $scope.toastList = function(message) {
+        var toast = $mdToast.simple().content('You clicked ' + message + ' having selected ' + $scope.selected.length + ' item(s)').position('bottom right');
+        $mdToast.show(toast);
+    };
+    $scope.selected = [];
+    $scope.toggle = function(item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) list.splice(idx, 1);
+        else list.push(item);
+    };
+    $scope.data = {
+        title: 'Trondheim Kommune',
+        user: {
+            name: 'Ola Nordmann',
+            email: 'ola@email.no',
+            icon: 'account_circle'
+        },
+        toolbar: {
+            buttons: [{
+                name: 'Button 1',
+                icon: 'add',
+                link: 'Button 1'
+            }],
+            menus: [{
+                name: 'Menu 1',
+                icon: 'message',
+                width: '4',
+                actions: [{
+                    name: 'Action 1',
+                    message: 'Action 1',
+                    completed: true,
+                    error: true
+                }, {
+                    name: 'Action 2',
+                    message: 'Action 2',
+                    completed: false,
+                    error: false
+                }, {
+                    name: 'Action 3',
+                    message: 'Action 3',
+                    completed: true,
+                    error: true
+                }]
+            }]
+        },
+        sidenav: {
+            sections: [{
+                expand: true,
+                actions: [{
+                    name: 'Hjem',
+                    icon: 'home',
+                    link: 'Hjem',
+                }, {
+                    name: 'Timelister',
+                    icon: 'assignment',
+                    link: 'Timelister'
+                }, {
+                    name: 'Vaktbytte',
+                    icon: 'swap_horiz',
+                    link: 'Vaktbytte'
+                }]
+            }, {
+                expand: true,
+                actions: [{
+                    name: 'Kontrollpanel',
+                    icon: 'build',
+                    link: 'Action 4'
+                }, {
+                    name: 'Brukere',
+                    icon: 'person',
+                    link: 'Action 5'
+                }, {
+                    name: 'Action 6',
+                    icon: 'settings',
+                    link: 'Action 6'
+                }]
+            }]
+        },
+        content: {
+            lists: [{
+                name: 'List 1',
+                menu: {
+                    name: 'Menu 1',
+                    icon: 'settings',
+                    width: '4',
+                    actions: [{
+                        name: 'Action 1',
+                        message: 'Action 1',
+                        completed: true,
+                        error: true
+                    }]
+                },
+                items: [{
+                    name: 'Item 1',
+                    description: 'Description 1',
+                    link: 'Item 1'
+                }, {
+                    name: 'Item 2',
+                    description: 'Description 2',
+                    link: 'Item 2'
+                }, {
+                    name: 'Item 3',
+                    description: 'Description 3',
+                    link: 'Item 3'
+                }]
+            }]
+        }
+    }
+});
