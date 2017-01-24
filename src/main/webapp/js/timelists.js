20906ea9b4aa0a9b0e7006ef60e27269a74bc8f2
Elena Nordmark <elenafalk@gmail.com>
2017-01-24 15:18:00 +0100

    timeoversikt for ansatte - fungerer

--- src/main/webapp/js/timelists.js
+++ src/main/webapp/js/timelists.js
@@ -2,18 +2,22 @@
 $(document).ready(function() {
     // Bind opp tabellen mot rest-ressursen
     var user = "nina"; //test
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
     var table = $('#table_timelists').DataTable( {
         "paging": false,
         "info" : false,
         "select" : true,
+
         ajax: {
             url: 'api/timelists/' + user,
             "dataSrc": function (json) {
                 var return_data = new Array();
                 for (var i = 0; i < json.length; i++) {
                     return_data.push({
-                        'month': json[i].month,
+                        'month': monthNames[json[i].month - 1],
                         'overtime': json[i].overtime,
                         'absence': json[i].absence,
                         'ordinary': json[i].ordinary,
