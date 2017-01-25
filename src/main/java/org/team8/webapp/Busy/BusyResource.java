96bc8c57ee1181cdb8bc4e57e7ad8b879f453f1a
masha <mariya.shchekanenko@gmail.com>
2017-01-25 12:44:23 +0100

    method to register when an employee is not available

--- src/main/java/org/team8/webapp/Busy/BusyResource.java
+++ src/main/java/org/team8/webapp/Busy/BusyResource.java
@@ -16,8 +16,8 @@ public class BusyResource{
         @GET
         @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
         public ArrayList<Busy> getBusy() {
-            System.out.println("getBusy");
-            return dao.getBusy();
+                System.out.println("getBusy");
+                return dao.getBusy();
         }
 
         @Path("{user_id}")
@@ -32,15 +32,15 @@ public class BusyResource{
         @GET
         @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
         public Busy getBusyByUserId(@PathParam("user_id") String user_id, @PathParam("shift_id") int shift_id) {
-            System.out.println("getSingleBusy: " + user_id + " " + shift_id);
-            return dao.getSingleBusy(user_id, shift_id);
+                System.out.println("getSingleBusy: " + user_id + " " + shift_id);
+                return dao.getSingleBusy(user_id, shift_id);
         }
 
         @POST
         @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
         public boolean createBusy(Busy b) {
-        System.out.println("createBusy");
-        return dao.createBusy(b);
+                System.out.println("createBusy");
+                return dao.createBusy(b);
         }
 
         @Path("{user_id}/{shift_id}")
@@ -54,6 +54,6 @@ public class BusyResource{
         @DELETE
         @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
         public void removeBusy(@PathParam("user_id") String user_id, @PathParam("shift_id") int shift_id) {
-            dao.removeBusy(user_id, shift_id);
+                dao.removeBusy(user_id, shift_id);
         }
 }
--- src/main/webapp/js/employee.js
+++ src/main/webapp/js/employee.js
@@ -327,4 +327,8 @@ $(document).ready(function() {
         registerOvertimeAbsence(user_id, shift_id, my_date, deviance);
     })
 
+    function getPhoneNumbers(){
+
+    }
+
 });
