02f91d579fa135a0b3bbd940c5dd320757abf129
espen <espenoss@gmail.com>
2017-01-25 14:54:33 +0100

    start my_page

--- src/main/webapp/js/localUserRole.js
+++ src/main/webapp/js/localUserRole.js
@@ -2,15 +2,8 @@
  * Created by espen on 23.01.2017.
  */
 
-function getMenu(){
-    if(sessionStorage.getItem("role") == "admin"){
-        // TODO: koble til ui, hent meny for admin
-    }else{
-        // TODO: koble til ui, hent meny for bruker
-    }
-}
 
-$(document).ready(function(){
+function getRole(callback){
     if(sessionStorage.getItem("role") == null){
         $.get("/api/users/current", function(user){
             if(user.role == 0){
@@ -18,9 +11,9 @@ $(document).ready(function(){
             }else{
                 sessionStorage.setItem("role", "user");
             }
-            getMenu();
+            callback();
         });
     }else{
-        getMenu();
+        callback();
     }
-});
+}
